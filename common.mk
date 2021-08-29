80773eea382ca979d25d28f9668100bd952172aa
Cosmin Tanislav <demonsingur@gmail.com>
2021-08-29 23:19:26 +0300

    DNM: sm8350-common: do not build display mapper

--- common.mk
+++ common.mk
@@ -133,12 +133,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-impl \
     android.hardware.graphics.composer@2.4-service \
-    android.hardware.graphics.mapper@3.0-impl-qti-display \
-    android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     vendor.qti.hardware.display.mapper@2.0.vendor
 #    vendor.qti.hardware.display.allocator-service \
+#    android.hardware.graphics.mapper@3.0-impl-qti-display \
+#    android.hardware.graphics.mapper@4.0-impl-qti-display \
 
 # DRM
 PRODUCT_PACKAGES += \
