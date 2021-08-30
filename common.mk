6550ad093951c8bf0c857cba66a58e188311d054
Cosmin Tanislav <demonsingur@gmail.com>
2021-08-30 17:53:58 +0300

    sm8350-common: add A/B postinstall scripts

--- common.mk
+++ common.mk
@@ -88,6 +88,19 @@ PRODUCT_PACKAGES += \
     r-gsi.avbpubkey \
     s-gsi.avbpubkey
 
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
