1b0e64b1831596081dddef53cb36407445ebc34e
Cosmin Tanislav <demonsingur@gmail.com>
2021-08-31 23:59:29 +0300

    DNM: sm8350-common: do not build livedisplay service

--- common.mk
+++ common.mk
@@ -261,10 +261,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sm8350
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libmm-omxcore \
