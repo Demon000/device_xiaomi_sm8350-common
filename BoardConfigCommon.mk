441ac4b524682177c58a412e28a06777f6db226e
Cosmin Tanislav <demonsingur@gmail.com>
2021-11-06 22:41:16 +0200

    sm8350-common: update security patch from 21.9.1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -228,7 +228,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-07-01
+VENDOR_SECURITY_PATCH := 2021-09-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
