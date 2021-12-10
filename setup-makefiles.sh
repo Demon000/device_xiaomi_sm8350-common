db6810b4d0512e0a8be5de1b6b4cd8f015a91fe7
Erfan Abdi <erfangplus@gmail.com>
2021-12-10 16:02:09 +0330

    sm8350-common: Add haydn support

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -26,7 +26,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
 
 # Copyright headers and guards
-write_headers "venus"
+write_headers "venus haydn"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
